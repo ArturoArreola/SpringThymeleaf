686feb5a68abfc87d377c5b836556edb082a3803
Nuuptech <nuup-user1@localhost.localdomain>
2019-03-20 17:07:13 -0600

    Primer commit proyecto

---
+++ InitializrSpringbootProject/src/test/java/com/nuuptech/demo/SpringBootAppJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nuuptech.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootAppJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
